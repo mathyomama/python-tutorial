478f0bd618d2b29b0f853d2768c0ceb86c906b0c
Kevin George <kevin.george@protonmail.com>
2016-07-19 11:25:10 -0400

    Adding source

---
+++ kevin/fibonacci.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+def fibonacci_sequence(number):
+    if number == 1:
+        print("0")
+    elif number == 2:
+        print("0,1")
+    else:
+        first = 0
+        second = 1
+        print("0,1", end = ",")
+        for x in range(2, number):
+            new_val = first + second
+            if x == number - 1:
+                print(new_val)
+            else:
+                print(new_val , end = ",")
+            first = second
+            second = new_val
+        return
+
+if __name__ == "__main__":
+    while True:
+        print("")
+        number = input("Enter the number for the Fibonacci sequence: ").strip()
+        if number != "":
+            number = int(number)
+            if number == 0:
+                break
+            fibonacci_sequence(number)
